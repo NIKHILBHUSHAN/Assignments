91008aa0baf0acd8138ccdcadcc9dd9bcb550fb6
NIKHILBHUSHAN <nikhilbhushan95824@gmail.com>
2026-04-14 21:20:23 +0530

    Handling messy data

---
+++ Handling_messy_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder,MinMaxScaler,StandardScaler
+#Task-1
+
+df = pd.DataFrame({
+    'customer_id': [1, 2, 3, 4, 2, 5],
+    'age':         [25, np.nan, 35, np.nan, np.nan, 40],
+    'city':        ['Mumbai', 'Delhi', np.nan, 'Mumbai', 'Delhi', np.nan],
+    'gender':      ['Male', 'Female', 'Female', 'Male', 'Female', 'Male'],
+    'annual_income': [40000, 80000, 60000, 120000, 80000, 95000]
+})
+
+df["age"].fillna(df["age"].mean(),inplace=True)
+df["city"].fillna(df["city"].mode()[0],inplace=True)
+
+df.drop_duplicates()
+
+#Task-2
+df=pd.get_dummies(df,columns="city")
+Le=LabelEncoder()
+df=Le.fit_transform(df["gender"])
+
+print("Encoded Data:",df)
+
+#Task-3
+df_minmax=df.copy()
+df_standard=df.copy()
+cols=['age', 'annual_income']
+
+minmax=MinMaxScaler()
+df_minmax[cols]=minmax.fit_transform(df[cols])
+
+
+print("\nMin-Max Scaled Data:\n", df_minmax)
+
+standard=StandardScaler()
+df_standard[cols]=standard.fit_transform(df[cols])
+
+print("\nStandard Scaled Data:\n", df_standard)
